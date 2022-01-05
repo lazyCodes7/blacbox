e8ca500fb9e2991b31e4766afe47903dac8ee9d4
lazyCodes7 <rishabmudliar@gmail.com>
2022-01-05 13:21:07 +0530

    Fix: Requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', encoding='utf-8') as fr:
 
 setup(
     name="blacbox",
-    version="0.1.0",
+    version="0.2.0",
     description="A visualization library to make CNNs more interpretable",
     long_description=long_description,
     long_description_content_type="text/markdown",
